69892351fca62b744fca746cb4a0f5ea9c0335dc
aliu1212 <alexliu12@gmail.com>
2018-12-08 12:18:57 -0800

    working on session

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -2,7 +2,22 @@
 
 const parseCookies = (req, res, next) => {
     let cookie_string = req.headers["cookie"];
-    console.log(cookie_string)
+    let cookie_obj = {};
+   // console.log(typeof cookie_string)
+   if (typeof cookie_string !== "undefined") {
+    cookie_array = cookie_string.split("; ")
+
+    //console.log(cookie_array);
+    for (let i=0; i<cookie_array.length; i++) {
+      let key = cookie_array[i].split("=")[0]
+      //console.log("key", key);
+      let value = cookie_array[i].split("=")[1];
+      //console.log("value", value);
+      cookie_obj[key] = value;
+    }
+  }
+  req.cookies = cookie_obj;
+  next()
 };
 
 module.exports = parseCookies;
